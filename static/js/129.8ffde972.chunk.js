5e5b0bdc1eee84d33222dabf55ae8838ba57678e
Artem-Grushchinskiy <122301492+Artem-Grushchinskiy@users.noreply.github.com>
2023-05-22 15:14:26 +0000

    Deploying to gh-pages from @ Artem-Grushchinskiy/goit-react-hw-08-phonebook@4a9e1ed8b35e346be6bb4d07f08701ba01c22f8e 🚀

---
+++ static/js/129.8ffde972.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[129],{2129:function(e,t,a){a.r(t);var n=a(1267),r=a(3329);t.default=function(){return(0,r.jsx)("h2",{className:n.Z.home__title,children:"The page is not found \ud83d\ude0e"})}}}]);
+//# sourceMappingURL=129.8ffde972.chunk.js.map
